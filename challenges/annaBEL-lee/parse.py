7b3c683cd36db0d2476d681409dcafdc9e9b85d4
lonkaars <loek@pipeframe.xyz>
2021-07-12 12:58:47 +0200

    parse wireshark capture done

--- challenges/annaBEL-lee/parse.py
+++ challenges/annaBEL-lee/parse.py
@@ -15,3 +15,19 @@ for line in lines:
   data = split[1].strip()[5:]
   packets.append({ 'time': time, 'data': data })
 
+def hexdump(bytes):
+  print("|", end="")
+  for byte in bytes:
+    n = int(byte, 16)
+    print(chr(n) if 32 <= n <= 127 else ".", end="")
+  print("|", end="")
+
+for packet in packets:
+  print(packet['time'] + "  ", end="")
+  bytes = packet['data'][1:-1].split("|")
+  for i in range(0, len(bytes), 16):
+    if i != 0: print(" " * ( len(packet['time']) + 2 ), end="")
+    print(' '.join(bytes[i:i+16]), end="")
+    print("  ", end="")
+    hexdump(bytes[i:i+16])
+    print("\n", end="")
